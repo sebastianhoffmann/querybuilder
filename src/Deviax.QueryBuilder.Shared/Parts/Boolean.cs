ec161f2c5122aba3ddac255a1e88e9688e4815f3
Sebastian Hoffmann <s.hoffmann@deviax.com>
2016-10-25 16:42:51 +0200

    fix(select): fix AppendTo for generic selects

--- src/Deviax.QueryBuilder.Shared/Parts/Boolean.cs
+++ src/Deviax.QueryBuilder.Shared/Parts/Boolean.cs
@@ -45,8 +45,8 @@ namespace Deviax.QueryBuilder.Parts
 
                 return this;
             }
-
-            public SelectQuery AppendTo(SelectQuery q) => _parts.Count == 0 ? q : q.Where(Build());
+            
+            public T AppendTo<T>(T q) where T : BaseSelectQuery<T> => _parts.Count == 0 ? q : q.Where(Build());
 
             public IBooleanPart Build()
             {
