362e49ff6495130c83c70f724dd731011ea082ae
Sebastian Hoffmann <s.hoffmann@deviax.com>
2017-01-06 10:42:30 +0100

    nuget prep

--- src/Deviax.QueryBuilder.Postgres/Properties/AssemblyInfo.cs
+++ src/Deviax.QueryBuilder.Postgres/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Deviax.QueryBuilder.Postgres")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SQL DSL / Micro ORM - PostgreSQL Driver")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Deviax.QueryBuilder.Postgres")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.1")]
-[assembly: AssemblyFileVersion("0.0.1.1")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
